d9fbef430691f5e110debbff498b8d0bdb9e2664
Chun Zen Marc Tse <32941251+MarcieMarc425@users.noreply.github.com>
2019-01-16 21:21:16 -0500

    Added responsive design for homepage

--- otesc-api/src/index.js
+++ otesc-api/src/index.js
@@ -16,55 +16,55 @@ app.use(express.static(path.join(__dirname, '/../client/build')));
 dotenv.config();
 
 mongoose.connect(
-	// // for testing
-	// process.env.MONGODB_URL
+    // for testing
+    process.env.MONGODB_URL,
 
-	// for build
-	process.env.MONGODB_URI,
-	{ useNewUrlParser: true }
+    // // for build
+    // process.env.MONGODB_URI,
+    { useNewUrlParser: true }
 );
 var db = mongoose.connection;
 db.once('open', () => {
-	console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 });
 
 app.get('*', (req, res) => {
-	// for build
-	res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+    // // for build
+    // res.sendFile(path.join(__dirname, '/../client/build/index.html'));
 
-	// //for testing
-	// res.sendFile(path.join(__dirname, 'index.html'));
+    //for testing
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/api/jobsearch', (req, res) => {
-	if (req.body.query.job === '' && req.body.query.location === '') {
-		Jobs.find({}, (err, docs) => {
-			res.json(docs);
-		});
-	}
+    if (req.body.query.job === '' && req.body.query.location === '') {
+        Jobs.find({}, (err, docs) => {
+            res.json(docs);
+        });
+    }
 });
 
 app.post('/api/subscribe', (req, res) => {
-	var subscribe = new Subscription({ email: req.body.email });
-	subscribe.save(err => {
-		if (!err) res.json('Success');
-		else res.json(err);
-	});
+    var subscribe = new Subscription({ email: req.body.email });
+    subscribe.save(err => {
+        if (!err) res.json('Success');
+        else res.json(err);
+    });
 });
 
 app.post('/api/msg', (req, res) => {
-	var msg = new Message({
-		name: req.body.msg.name,
-		email: req.body.msg.email,
-		phone: req.body.msg.phone,
-		text: req.body.msg.text
-	});
-	msg.save(err => {
-		if (!err) res.json('Success');
-		else res.json(err);
-	});
+    var msg = new Message({
+        name: req.body.msg.name,
+        email: req.body.msg.email,
+        phone: req.body.msg.phone,
+        text: req.body.msg.text
+    });
+    msg.save(err => {
+        if (!err) res.json('Success');
+        else res.json(err);
+    });
 });
 
 app.listen(process.env.PORT || 5000, () =>
-	console.log('Running on localhost:5000')
+    console.log('Running on localhost:5000')
 );
--- otesc-react/src/components/Company/Contact/contact.js
+++ otesc-react/src/components/Company/Contact/contact.js
@@ -3,22 +3,22 @@ import NavBar from '../../Jobpage/navBar/navbar';
 import Hero from '../Hero/hero';
 import Info from '../Info/info';
 import Footer from '../../Homepage/footer/footer';
-
+import './contact.scss';
 class contact extends Component {
-	componentDidMount = () => {
-		window.scrollTo(0, 0);
-	};
+    componentDidMount = () => {
+        window.scrollTo(0, 0);
+    };
 
-	render() {
-		return (
-			<div className='contact'>
-				<NavBar />
-				<Hero title='Contact Us' />
-				<Info type='contact' />
-				<Footer />
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className='contact'>
+                <NavBar className='contact-navbar' />
+                <Hero title='Contact Us' />
+                <Info type='contact' className='contact-info' />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default contact;
